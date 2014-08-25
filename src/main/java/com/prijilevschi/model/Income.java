d73a66038d642a3cdea3d5293178d776915ec2d1
Evgheni Prijilevschi <Evghenii.Prijilevschi@endava.com>
2014-08-25 15:46:56 +0300

    Refactoring

--- src/main/java/com/prijilevschi/model/Income.java
+++ src/main/java/com/prijilevschi/model/Income.java
@@ -2,6 +2,7 @@ package com.prijilevschi.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -11,5 +12,6 @@ public class Income extends AbstractDatedBudgetaryEntity {
     private static final long serialVersionUID = 7030205425880527993L;
 
     @Column
+    @OneToOne
     private IncomeType incomeType;
 }
--- src/main/java/com/prijilevschi/model/IncomeType.java
+++ src/main/java/com/prijilevschi/model/IncomeType.java
@@ -1,15 +1,8 @@
-/*******************************************************************************
-*
-* © Copyright by Endava 2014.
-*
-* All Rights Reserved
-*
-* Endava – Internal Use Only
-*
-*******************************************************************************/
 package com.prijilevschi.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,4 +10,15 @@ import javax.persistence.Table;
 public class IncomeType extends AbstractNamedEntity {
     private static final long serialVersionUID = -1421496095532548801L;
 
+    @OneToOne
+    @Column
+    private Income income;
+
+    public Income getIncome() {
+        return income;
+    }
+
+    public void setIncome(Income income) {
+        this.income = income;
+    }
 }
