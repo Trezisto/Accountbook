1fafbb71a7bb6e8749b764c780a2960cc43548d3
Evghenii Prijilevschi <prijilevschi.evgheni@gmail.com>
2015-08-24 23:59:08 +0300

    Apply custom Primefaces theme

--- src/main/java/com/prijilevschi/controller/LoginController.java
+++ src/main/java/com/prijilevschi/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.prijilevschi.controller;
 
 import java.io.Serializable;
+import java.net.ConnectException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
@@ -11,47 +12,54 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.transaction.CannotCreateTransactionException;
+
 import com.prijilevschi.model.User;
 import com.prijilevschi.service.UserService;
 
 @ManagedBean(name = "loginController")
 @RequestScoped
-public class LoginController implements Serializable{
+public class LoginController implements Serializable {
 	private static final long serialVersionUID = 3285542075160514743L;
-	
+
 	private String username;
 	private String password;
-	
+
 	@ManagedProperty(value = "#{userService}")
 	private transient UserService userService;
-	
-	public String performLogin(){
-		try{
-//			User user = new User();
-//			user.setUserName("test");
-//			user.setPassword("test");
-//
-//			Long id = userService.save(user); //constraint violation
+
+	public String performLogin() {
+		try {
+			// User user = new User();
+			// user.setUserName("test");
+			// user.setPassword("test");
+			//
+			// Long id = userService.save(user); //constraint violation
 			boolean result = userService.login(username, password);
 
-			if(result){
+			if (result) {
 				HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 				session.setAttribute("username", username);
 
 				return "index";
 			} else {
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalid Login", "Please Try Again!"));
-
+				FacesContext.getCurrentInstance().addMessage(null,
+						new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalid Login", "Please Try Again!"));
+				
 				return "login";
 			}
-		} catch(NoSuchAlgorithmException | InvalidKeySpecException e){ 
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Exception Occured", e.getMessage()));
-
-			return "login";
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Exception Occured", e.getMessage()));
+		} catch (CannotCreateTransactionException e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Could not create connection to database server!", e.getMessage()));
 		}
+		return "login";
+
 	}
-	
-	public String logout(){
+
+	public String logout() {
 		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 		session.invalidate();
 		return "login";
