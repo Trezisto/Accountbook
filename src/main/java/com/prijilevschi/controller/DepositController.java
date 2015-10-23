d7e98a85e99bcc445591363094acea1ea2df92d5
Evgheni Prijilevschi <prijilevschi.evgheni@gmail.com>
2015-10-23 16:48:04 +0300

    Revert old development

--- src/main/java/com/prijilevschi/controller/DepositController.java
+++ src/main/java/com/prijilevschi/controller/DepositController.java
@@ -6,9 +6,9 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name = "financesController")
+@ManagedBean(name = "depositController")
 @RequestScoped
-public class FinancesController implements Serializable {
+public class DepositController implements Serializable {
     private static final long serialVersionUID = 5930564554393379903L;
 
     @PostConstruct
